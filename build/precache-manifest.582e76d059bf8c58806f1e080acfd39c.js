e8963e2a78523524ed6683863def543d128fc878
utlime <utlime@gmail.com>
2019-02-10 18:13:28 +0700

    dictionary

---
+++ build/precache-manifest.582e76d059bf8c58806f1e080acfd39c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d9b763927c927ab68022",
+    "url": "/static/js/main.d9b76392.chunk.js"
+  },
+  {
+    "revision": "8b74a510f5b908037d66",
+    "url": "/static/js/1.8b74a510.chunk.js"
+  },
+  {
+    "revision": "625f770d6b369375410307d5e51c4913",
+    "url": "/index.html"
+  }
+];
